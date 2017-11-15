00644821bcb0655e7b38b29706db1beb6fc56d73
allen <afreetime@gmail.com>
2017-11-15 14:00:10 +0800

    init hello world package

---
+++ src/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/15 015
+ * Time: 13:36
+ */
+
+namespace Afreetime\FirstProject;
+
+class Test
+{
+    public function HelloWorld()
+    {
+        echo "Hello world.";
+    }
+}
